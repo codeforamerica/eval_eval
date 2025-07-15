354bf38da72f5d50b25a9aaa6fb671b0d2fdce15
Lauren Kemperman <lkemperman@codeforamerica.org>
2025-07-15 14:42:17 -0400

    extend the schema to add optional context (list of strings)

---
+++ experiments/opik_experiment.py
@@ -0,0 +1,47 @@
+from typing import List
+
+from opik.evaluation.metrics import Hallucination
+from deepeval.models import OllamaModel
+
+from eval_eval.evaluation import MetricExperimentBase
+from eval_eval.logger import logger
+from eval_eval.schema import Analysis, EvaluationResult
+
+"""
+Implements metrics from the Opik framework.
+
+Resources:
+- [Main Documentation](https://www.comet.com/docs/opik/
+- [Repo](https://github.com/comet-ml/opik)
+- [Hallucination Metric](https://www.comet.com/docs/opik/evaluation/metrics/hallucination)
+"""
+
+
+class OpikHallucinationExperiment(MetricExperimentBase):
+    METRIC_NAME = "opik_eval_hallucination"
+
+    @staticmethod
+    def run_eval(
+            analysis: Analysis, notice_text: str, notice_path: str
+    ) -> EvaluationResult | List[EvaluationResult]:
+        # Add an ID to analysis parts.
+        model = OllamaModel("deepseek-r1:8b")
+        text_to_evaluate = [("summary", analysis.summary)]
+        for item in analysis.questions:
+            text_to_evaluate.append((item.question, item.answer, item.context))
+        metric = Hallucination()
+
+        results = []
+        for i, text in enumerate(text_to_evaluate):
+            logger.info(f"Opik Hallucination: Evaluating step {i + 1} of {len(text_to_evaluate)}")
+
+            metric.score(input=text[0], output=text[1], context=text[2])
+            results.append(
+                EvaluationResult(
+                    metric_name=OpikHallucinationExperiment.METRIC_NAME,
+                    score=metric.score,
+                    llm_model_name=model.model_name,
+                    related_analysis=text[0],
+                )
+            )
+        return results
